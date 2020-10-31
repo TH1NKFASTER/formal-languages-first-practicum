12a9f6d328119af8921d9dd9e43b88dc7f8db636
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-11-01 00:27:08 +0300

    stuff

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/maksimdolgov/CLionProjects/formal_languages_practicum
-# Build directory: /Users/maksimdolgov/CLionProjects/formal_languages_practicum/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/maksimdolgov/CLionProjects/formal_languages_practicum/Google_tests/lib
-# Build directory: /Users/maksimdolgov/CLionProjects/formal_languages_practicum/cmake-build-debug/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
